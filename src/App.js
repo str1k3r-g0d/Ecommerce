79d9030adf2952ca2ed5ce3ca0497f67bf7b0cb0
str1k3r-g0d <yash.raj021@gmail.com>
2019-09-15 12:27:03 +0530

    3.3. Added Sign-in page with styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Homepage from './pages/homepage/homepage.pages'
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.pages.jsx'
 import Header from './components/header.component/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up.pages/sign-in-and-sign-up.pages';
+
 
 import './App.css';
 
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component ={ Homepage }/>
         <Route path='/shop' component ={ ShopPage }/>
+        <Route path = '/signin' component = { SignInAndSignUpPage }/>
       </Switch>
     </div>
   );
