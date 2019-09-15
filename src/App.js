90236513da9b7f04a4a5c17733a588879300e1c7
str1k3r-g0d <yash.raj021@gmail.com>
2019-09-15 11:25:56 +0530

    3.1. Added Header. Included Routing.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Homepage from './pages/homepage/homepage.pages'
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.pages.jsx'
+import Header from './components/header.component/header.component';
+
 import './App.css';
 
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component ={ Homepage }/>
-        <Route path='/hats' component ={ ShopPage }/>
+        <Route path='/shop' component ={ ShopPage }/>
       </Switch>
     </div>
   );
