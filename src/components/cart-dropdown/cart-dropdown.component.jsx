eaf65f84a7d5bdf73098abdf3e6eca5f867395d1
str1k3r-g0d <yash.raj021@gmail.com>
2019-12-11 22:40:37 +0530

    Added Memorized selectors to replace usual selectors to prevent re-rendering of unrelated components.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import './cart-dropdown.styles.scss';
 import CustomButtom from '../custom-button.component/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 const CartDropdown = ({cartItems}) => (
 
@@ -19,8 +20,8 @@ const CartDropdown = ({cartItems}) => (
     
 );
 
-const mapStateToProps = ({cart: {cartItems}})=> ({ 
-    cartItems,
+const mapStateToProps = (state)=> ({ 
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactComponent as ShoppingIcon } from './shopping-bag.svg';
 import {connect} from 'react-redux';
 import {toogleCartHidden} from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './cart-icon.styles.scss';
 
@@ -20,9 +21,8 @@ const mapDispatchToProps = dispatch => ({
     toogleCartHidden: () => dispatch(toogleCartHidden())
 })
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity 
-    , 0)
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
